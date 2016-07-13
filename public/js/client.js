23273f914b3d8ae56168119e43562233dc1bea51
Andrew Kolander <akolander22@gmail.com>
2016-07-13 16:01:40 -0500

    updated with accurate controllers;

--- public/js/client.js
+++ public/js/client.js
@@ -25,46 +25,34 @@ angular.module('olympicApp').config(function($routeProvider, $locationProvider){
   $locationProvider.html5Mode(true);
 });
 
-angular.module('olympicApp').controller('MainController', function($scope, $http){
 
-
-  $scope.addAthletes = function(){
-    $http.post('/olympics/judo').then(function(response){
-      $scope.randoJudo = response.data[Math.floor(Math.random()*response.data.length)];
-      // console.log(randoJudo);
-    })
-    $http.post('/olympics/badminton').then(function(response){
-      $scope.randoBadminton = response.data[Math.floor(Math.random()*response.data.length)];
-      // console.log(randoBadminton);
-    })
-    $http.post('/olympics/archery').then(function(response){
-      $scope.randoArchery = response.data[Math.floor(Math.random()*response.data.length)];
-      // console.log(randoArchery);
-    })
-    $http.post('/olympics/diving').then(function(response){
-      $scope.randoDiving = response.data[Math.floor(Math.random()*response.data.length)];
-      // console.log(randoDiving);
-    })
-    $http.post('/olympics/tableTennis').then(function(response){
-      $scope.randoTableTennis = response.data[Math.floor(Math.random()*response.data.length)];
-      // console.log(randoTableTennis);
-    })
-  }
-
-})
-
-angular.module('olympicApp').controller('JudoController', function($scope){
-
+angular.module('olympicApp').controller('JudoController', function($scope, $http){
+  $http.post('/olympics/judo').then(function(response){
+    $scope.randoJudo = response.data[Math.floor(Math.random()*response.data.length)];
+    // console.log(randoJudo);
+  })
 });
-angular.module('olympicApp').controller('BadmintonController', function($scope){
-
+angular.module('olympicApp').controller('BadmintonController', function($scope, $http){
+  $http.post('/olympics/badminton').then(function(response){
+    $scope.randoBadminton = response.data[Math.floor(Math.random()*response.data.length)];
+    // console.log(randoBadminton);
+  })
 });
-angular.module('olympicApp').controller('DivingController', function($scope){
-
+angular.module('olympicApp').controller('DivingController', function($scope, $http){
+  $http.post('/olympics/diving').then(function(response){
+    $scope.randoDiving = response.data[Math.floor(Math.random()*response.data.length)];
+    // console.log(randoDiving);
+  })
 });
-angular.module('olympicApp').controller('ArcheryController', function($scope){
-
+angular.module('olympicApp').controller('ArcheryController', function($scope, $http){
+  $http.post('/olympics/archery').then(function(response){
+    $scope.randoArchery = response.data[Math.floor(Math.random()*response.data.length)];
+    // console.log(randoArchery);
+  })
 });
-angular.module('olympicApp').controller('TableTennisController', function($scope){
-
+angular.module('olympicApp').controller('TableTennisController', function($scope, $http){
+  $http.post('/olympics/tableTennis').then(function(response){
+    $scope.randoTableTennis = response.data[Math.floor(Math.random()*response.data.length)];
+    // console.log(randoTableTennis);
+  })
 });
